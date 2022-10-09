406326f7ac8ece3e0760f19e007e38bbdfd2af62
jegunderson <jegunderson@byui.edu>
2022-10-08 21:15:22 -0600

    Chamber of Commerce home page

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,4 @@ const today = new Date();
 document.querySelector("footer div span").textContent = today.getFullYear();
 // document.getElementById("lastupdatedate").textContent = document.lastModified
 const lastmodified = new Date(document.lastModified);
-document.getElementById("lastupdatedate").textContent = `${lastmodified.getDate()}/${lastmodified.getMonth()+1}/${lastmodified.getFullYear()} :-)`;
+document.getElementById("lastupdatedate").textContent = `${lastmodified.getDate()}/${lastmodified.getMonth()+1}/${lastmodified.getFullYear()} ${lastmodified.getHours()}:${lastmodified.getMinutes()}:${lastmodified.getSeconds()} :-)`;
---
+++ lesson04/js/lesson04.js
@@ -0,0 +1,13 @@
+
+// const hamburgerbtn = document.querySelector('.ham');
+// const navigation = document.querySelector('.navigation')
+
+// hamburgerbtn.addEventListener('click'), () => {navigation.classList}
+
+function togglemenu() {
+    // console.log("It worked!");
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerbtn')
+x.onclick = togglemenu;
