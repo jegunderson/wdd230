74233d5d13de24f349c40d32c0df0426fa40f28f
jegunderson <jegunderson@byui.edu>
2022-10-12 16:27:44 -0600

    change chamber link

---
+++ lesson05/js/bom.js
@@ -0,0 +1,30 @@
+const myInput = document.getElementById("favchap");
+const myList = document.querySelector(".list");
+const myButton = document.querySelector("main div button");
+
+myButton.addEventListener("click", () => {
+    if (myInput.value == ""){
+        return;
+    }
+    const newItem = document.createElement("li");
+
+    const newButton = document.createElement("button");
+
+    newItem.textContent = myInput.value;
+
+    newButton.textContent = "❌";
+
+    newItem.append(newButton);
+
+    myList.append(newItem);
+
+    newButton.addEventListener("click", () => {
+        myList.remove();
+    });
+
+    
+    myInput.focus();
+
+    myInput.value = "";
+
+});
