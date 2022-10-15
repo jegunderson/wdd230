8e1a4344daeb10ab0548fbc20e2ad22e5ce282c8
jegunderson <jegunderson@byui.edu>
2022-10-15 14:48:23 -0600

    page formatting

--- Chamber/js/lesson04.js
+++ Chamber/js/lesson04.js
@@ -12,13 +12,20 @@ function togglemenu() {
 const x = document.getElementById('hamburgerbtn')
 x.onclick = togglemenu;
 
-const date = Date.now();
-
-document.getElementById("datep").innerHTML(date);
-
-
 
 const today = new Date();
+document.querySelector("footer div span").textContent = today.getFullYear();
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let day = days[today.getDay()];
+let month = months[today.getMonth()]
+
+
+document.getElementById("datep").textContent = `${day}, ${today.getDate()} ${month} ${today.getFullYear()}`;
+
+const lastmodified = new Date(document.lastModified);
+document.getElementById("lastupdatedate").textContent = `${lastmodified.getDate()}/${lastmodified.getMonth()+1}/${lastmodified.getFullYear()} ${lastmodified.getHours()}:${lastmodified.getMinutes()}:${lastmodified.getSeconds()} :-)`;
 
 if (today.getDay() == 4){
     document.querySelector("header P").getElementsByClassName.display='block';
