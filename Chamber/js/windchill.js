0b938fe460869e48310031f93024e5e927329b21
jegunderson <jegunderson@byui.edu>
2022-10-22 13:05:56 -0600

    responsive images

--- Chamber/js/windchill.js
+++ Chamber/js/windchill.js
@@ -1,6 +1,6 @@
 function setWindChill(temp, windspeed)
-    // let temp = 49;
-    // let windspeed = 10;
+    let temp = 49;
+    let windspeed = 10;
 
     let tempobj = document.querySelector("#temperature");
     let windspeedobj = document.querySelector("#windspeed");
@@ -9,7 +9,7 @@ function setWindChill(temp, windspeed)
     let windchillmsg = "N/A";
 
     if (temp <= 50 && windspeed > 3){
-        let chill = Math.round((35.74 + (0.6215 * temp)) - (35.75))
+        let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
         windchillmsg = `${chill}&deg; F`;
 
     }
